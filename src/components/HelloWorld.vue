f6464c4050c04ac4290ba306c8df79641b1702c0
LEPRIC0NO92 <justielepric7512@list.ru>
2022-02-11 22:48:51 +0300

    Hello world

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <span>
+      Hello world
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
